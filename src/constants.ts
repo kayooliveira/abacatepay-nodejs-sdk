4002f0109682f9fa724331a6ca1270985dc615a7
Nicolas Melo <nicolasmelo12@gmail.com>
2024-12-09 14:39:01 -0300

    change docs for abacatepay

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { ABACATE_PAY_VERSION } from './version';
 
 export const BASE_URL = 'https://api.abacatepay.com/v1';
-export const ABACATE_PAY_DOCS = 'https://www.abacatepay.com/';
+export const ABACATE_PAY_DOCS = 'https://abacatepay.readme.io/reference/oi';
 export function GET_DEFAULT_HEADERS(apiKey: string) {
 	return {
 		Authorization: `Bearer ${apiKey}`,
